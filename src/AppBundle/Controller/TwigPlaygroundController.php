c267835f50fa3f3cbe4c5d260d9b04a7cecf7562
Samuel Besnier <sbesnier1901@gmail.com>
2017-06-28 16:42:51 +0200

    add cave

--- src/AppBundle/Controller/TwigPlaygroundController.php
+++ src/AppBundle/Controller/TwigPlaygroundController.php
@@ -17,10 +17,51 @@ class TwigPlaygroundController extends Controller
             "un évier en fer",
             "un pistolet à gaufres"
         ];
+
+        $person = [
+            "name" => "Besnier",
+            "firstname" => "Samuel",
+            "address" => [
+                "path" => "16 clos Picasso",
+                "zipcode" => "59310",
+                "city" => "Orchies"
+            ]
+        ];
+
+        $cave = [
+            [
+                "name" => "Poully fumé",
+                "origin" => "Bourgogne",
+                "cepage" => "Chardonay"
+            ],
+            [
+                "name" => "Mouton Cadet",
+                "origin" => "Bordeau",
+                "cepage" => "Cabernet"
+            ],
+            [
+                "name" => "Arbois Pupillin",
+                "origin" => "Arbois",
+                "cepage" => "Chardonay"
+            ],
+            [
+                "name" => "Vin jaune",
+                "origin" => "Arbois",
+                "cepage" => "Savagnin"
+            ],
+            [
+                "name" => "Nuit Saint Georges",
+                "origin" => "Bourgogne",
+                "cepage" => "Pinot noir"
+            ]
+        ];
+
         return $this->render('AppBundle:TwigPlayground:playground.html.twig', [
             "nom" => "Eli Lotar",
             "now" => new \DateTime(),
-            "inventary" => $inventary
+            "inventary" => $inventary,
+            "client" => $person,
+            "cave" => $cave
         ]);
     }
 
