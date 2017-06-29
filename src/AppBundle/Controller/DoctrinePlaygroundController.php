45900ac8dbec8280941578271dca08d1704aece7
Samuel Besnier <sbesnier1901@gmail.com>
2017-06-29 17:01:51 +0200

    generate entity from database

--- src/AppBundle/Controller/DoctrinePlaygroundController.php
+++ src/AppBundle/Controller/DoctrinePlaygroundController.php
@@ -11,37 +11,64 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
  * @package AppBundle\Controller
  *
  * @Route(
- *     "/posts/"
+ *     "/posts"
  * )
  */
 class DoctrinePlaygroundController extends Controller
 {
     /**
      * @Route(
-     *     "/insert-post"
+     *     "/insert-post",
      *     name="insert_post"
      * )
      */
     public function insertPostAction()
     {
-        // Post creation
-        $post = new Post();
-        $post->setTitle("Symfony c'est super");
-        $post->setContent("Le texte de l'article");
-        $post->setCreatedAt(new \DateTime());
+        // Get all posts
+        $repo = $this->getDoctrine()->getRepository('AppBundle:Post');
 
         // Get entity manager
         $em = $this->getDoctrine()->getManager();
 
-        // Persist handling
-        $em->persist($post);
+        $title = "Symfony c'est super";
+
+        // Search for post before create it
+        $search = $repo->findOneByTitle($title);
+
+        $post = new Post();
+
+        if (! $search ) {
+            // Post creation
+            $post->setTitle($title);
+            $post->setContent("Le texte de l'article");
+            $post->setCreatedAt(new \DateTime());
+
+            // Persist handling
+            $em->persist($post);
+        }
+
+        // Delete post where id is 2
+        $postToRemove = $repo->find(2);
+        if ( $postToRemove ) {
+            $em->remove($postToRemove);
+        }
+
+        // Change post title where id is 3
+        $postToChangeTitle = $repo->find(3);
+        if ( $postToChangeTitle ) {
+            $postToChangeTitle->setTitle("Symfony 3 c'est encore mieux");
+            $em->persist($postToChangeTitle);
+        }
 
         // Finalize transaction
         $em->flush();
 
-        return $this->render('AppBundle:DoctrinePlayground:insert_post.html.twig', array(
+        $posts = $repo->findAll();
 
-        ));
+        return $this->render('AppBundle:DoctrinePlayground:insert_post.html.twig', [
+            "post" => $post,
+            "allPosts" => $posts
+        ]);
     }
 
 }
